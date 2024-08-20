278ea432a26acebf709c769b15386feef0db86c0
Gleydson Brito <sicknessbaby@gmail.com>
2024-08-20 14:59:45 -0300

    Os três exercícios estão completos

--- aula04/10informações.py
+++ aula04/10informações.py
@@ -1,2 +1,17 @@
 # Receba 10 informações de cadastro pessoal e informe um relatorio com as informações em um unico print.
 
+print('Vamos agora coletar algumas informações sobre você. Responda as perguntas abaixo.')
+
+nome = str(input('Digite o seu nome: '))
+endereco = str(input('Digite o seu endereço: '))
+telefone = int(input('Digite o número do seu telefone: '))
+email = str(input('Digite o seu e-mail: '))
+altura = float(input('Digite a sua altura: '))
+diaNascimento = int(input('Digite o dia do seu nascimento, por exemplo; 14: '))
+mesNascimento = str(input('Digite o mês do seu nascimento, por exemplo; janeiro: '))
+anoNascimento = int(input('Digite o ano do seu nascimento, por exemplo; 2005: '))
+idade = str(input('Digite a sua idade : '))
+cidadeNascimento = str(input('Diga a cidade em que nasceu: '))
+estadoNascimento = str(input('Em qual Estado fica a cidade que nasceu?: '))
+
+print(f'Seu nome é: {nome} seu endereço é: {endereco} o seu telefone é: {telefone} seu e-mail é: {email} sua altura é: {altura} você nasceu no dia {diaNascimento} no mês de {mesNascimento} no ano de {anoNascimento} e sua idade é: {idade} a cidade que você nasceu foi {cidadeNascimento} e o estado no qual nasceu foi {estadoNascimento} ')
--- aula04/algoritmoBaskara.py
+++ aula04/algoritmoBaskara.py
@@ -1,1 +1,16 @@
 # crie um algoritimo capaz de calcular o delta da formula de bascara
+import math
+
+
+print('Vamos calcular o Delta da fóruma de Báskara')
+
+valorA = float(input('Digite o valor de A: '))
+valorB = float(input('Digite o valor de B: '))
+valorC = float(input('Digite o valor de C: '))
+
+print('Lembrando que a fórmula do Delta de Báskara é B^2-4xAxC, vamos calcular.')
+
+raiz = math.sqrt(valorB)
+calculo1 = (raiz) - (4 * valorA * valorC)
+
+print('O valor de Delta é: ', calculo1)
