8640b32808fb00e04fb5e09b19a918bd066d98ae
Gleydson Brito <sicknessbaby@gmail.com>
2024-08-20 17:55:12 -0300

    Salvando a aula 05

---
+++ aula05/aula05.py
@@ -0,0 +1,96 @@
+
+# numero1 = 10
+# numero2 = 10
+
+# #operador maior
+# print(numero1 > numero2)
+# print(numero2 > numero1)
+
+# #operador menor
+# print(numero1 < numero2)
+# print(numero2 < numero1)
+
+# #operador igual, maior que, menor que e diferente de
+# print(numero1 == numero2)
+# print(numero1 >= numero2)
+# print(numero1 <= numero2)
+# print(numero1 != numero2)
+
+# O operador condicional "if" significa "se" em português. Ele é usado para comparar as perguntas, como: "Se" o primeiro número é maior ">" que o outro número, me mostre
+# na tela "print" a resposta.
+# O operador condicional "else" dignifica "senão" em português. Ele é usado para dar uma resposta caso o uso do "if" não seja verdadeiro. Ele é usado sempre no final da pergunta.
+# O operador condicional "elif" é a junção do "else" e "if" que significa "senão se". Ele é utilizado para quando a pergunta tem mais de duas condições. É utilizado sempre entre o "if" e o "else".
+# Exemplo: Se uma pessoa é maior de 18 ou não e se tem uma idade máxima definida.
+
+
+# idade = int(input('Informe a sua idade: \n'))
+# if(idade > 120):
+#     print('Morre diabo!')
+# elif(idade >= 18):
+#     print('Maior de idade')
+# elif(idade < 0):
+#     print('Ainda não nasceu')    
+# else:
+#     print('Menor de idade')
+
+
+# idade = int(input('Informe a sua idade: \n'))
+
+
+# if(idade >= 18):
+#     print('Pode assistir o filme')
+# elif(idade >= 16):
+#     acompanhado = input(str('Está acompanhado de um adulto sim ou não? \n'))
+#     if(acompanhado == 'sim'):
+#         print('pode assistir')
+#     else:
+#         print('não pode assistir')
+# else:
+#     print
+#     ('não pode assistir')                
+
+# idade = int(input('Informe a sua idade: \n'))
+# if(idade < 18):
+#     acompanhado = input(str('Está acompanhado de um adulto sim ou não? \n'))
+#     if(acompanhado == 'sim'):
+#         print('pode assistir')
+#     else:
+#         print('não pode assistir')
+# else:
+#     print('pode assistir')            
+
+# Essa é a programação da tabela verdade do "and" onde só é verdade quando as duas respostas são sim.
+
+# aladdin = input('Aladdin apareceu? \n')
+# jasmine = input('Jasmine apareceu? \n')
+
+# if(aladdin == 'sim') and (jasmine == 'sim'):
+#     print('Love a noite inteira')
+# else:
+#     print('Não rolou encontro')
+
+# Essa é a programação da tabela verdade no "or" onde só é verdade quando uma resposta está certa ou as duas respostas estão certas.
+
+# aladdin = input('Aladdin apareceu? \n')
+# jasmine = input('Jasmine apareceu? \n')
+
+
+# if(aladdin == 'sim') or (jasmine == 'sim'):
+#     print('Love a noite inteira')
+# else:
+#     print('não rolou encontro')
+
+# Essa é a programação para a negação da tabela verdade. Só colocar entre parêntese e ante dele colocar a palavra "not"
+
+# aladdin = input('Aladdin apareceu? \n')
+# jasmine = input('Jasmine apareceu? \n')
+
+
+# if not ((aladdin == 'sim') or (jasmine == 'sim')):
+#     print('Love a noite inteira')
+# else:
+#     print('não rolou encontro')
+
+
+
+
