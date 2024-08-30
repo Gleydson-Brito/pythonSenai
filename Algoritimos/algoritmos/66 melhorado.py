e6365a8dad60b593a435c29e1b3d7eb49d5f9100
Gleydson Brito <sicknessbaby@gmail.com>
2024-08-30 17:43:57 -0300

    Jogo da velha completo, mas cabuloso de entender

---
+++ Algoritimos/algoritmos/66 melhorado.py
@@ -0,0 +1,36 @@
+from os import system
+import operacoes as op
+
+# from operacoes import menu, listar_nomes
+# import operacoes
+
+operacao = 'sim'
+
+
+while operacao == 'sim':
+    op.menu()
+    
+    opcao = int(input('Informe a ação desejada: '))
+
+    match opcao:
+        case 1:
+            nome = input('Qual nome deseja cadastrar? \n')
+            op.cadastrar_nome(nome)
+        case 2:
+           nome = input('Qual nome deseja atualizar? \n')
+           novo_nome = input('Qual será o novo nome? \n')
+           op.atualiza_nome(nome, novo_nome)
+        case 3:
+            nome = input('Qual nome deseja excluir? \n')
+            op.excluir_nome(nome)
+        case 4:
+            op.listar_nomes()
+            
+        case _:
+            print('opção inválida')
+    operacao = input('Deseja realizar outra operação? \n').lower()
+    system('clear')
+
+    if operacao != 'sim':
+        break
+    
---
+++ Exercicios65/exercicio66 melhorado.py
@@ -0,0 +1,44 @@
+import os
+
+nomes = []
+operacao = 'sim'
+
+def menu():
+    opcoes = ['Cadastrar nome', 'Atualizar nome', 'Excluir nome', 'Listar nome']
+
+    for indice, opcao in enumerate(opcoes):
+        print(f'{indice + 1} - {opcao}')
+
+def listar_nomes():
+    for indice, nome in enumerate(nomes):
+             
+        print(f' {indice} - {nome}')
+
+
+while operacao == 'sim':
+    menu()
+    
+    opcao = int(input('Informe a ação desejada: '))
+
+    match opcao:
+        case 1:
+            nome = input('Qual nome deseja cadastrar? \n')
+            nomes.append(nome)
+        case 2:
+           nome = input('Qual nome deseja atualizar? \n')
+           novo_nome = input('Qual será o novo nome? \n')
+           nomes[nomes.index(nome)] = novo_nome
+        case 3:
+            nome = input('Qual nome deseja excluir? \n')
+            nomes.remove(nome)
+        case 4:
+            listar_nomes()
+            
+        case _:
+            print('opção inválida')
+    operacao = input('Deseja realizar outra operação? \n').lower()
+    os.system('clear')
+
+    if operacao != 'sim':
+        break
+    
